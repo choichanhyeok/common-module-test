31265d142399bce2dd08f087dd339219c2a1a1b8
hyeoKing <cksgurwkd12@naver.com>
2024-06-16 23:52:30 +0900

    - JSON properties 적용

--- src/main/java/com/supermarket/common/payment/request/PaymentRequest.java
+++ src/main/java/com/supermarket/common/payment/request/PaymentRequest.java
@@ -1,12 +1,16 @@
 package com.supermarket.common.payment.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class PaymentRequest {
+    @JsonProperty("orderId")
     private final Long orderId;
+    @JsonProperty("customerId")
     private final String customerId;
+    @JsonProperty("paymentAmount")
     private final double paymentAmount;
 }
--- src/main/java/com/supermarket/common/payment/response/PaymentResponse.java
+++ src/main/java/com/supermarket/common/payment/response/PaymentResponse.java
@@ -1,12 +1,15 @@
 package com.supermarket.common.payment.response;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class PaymentResponse {
+    @JsonProperty("paymentStatus")
     private final String paymentStatus;
+    @JsonProperty("responseMessage")
     private final String responseMessage;
 }
