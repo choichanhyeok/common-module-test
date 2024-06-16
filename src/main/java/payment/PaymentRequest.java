a44ea2347ff7b3c1d2b32b3d7335731cff6cdbb8
hyeoKing <cksgurwkd12@naver.com>
2024-06-16 22:08:05 +0900

    공통 모듈 등록 테스트

---
+++ src/main/java/payment/PaymentRequest.java
@@ -0,0 +1,12 @@
+package payment;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PaymentRequest {
+    private final Long orderId;
+    private final String customerId;
+    private final double paymentAmount;
+}
